8fa640fc8ed2a0220ad27fe1c736d0b4a82d98fe
ThiagoFragaK <thiagofraga@proton.me>
2025-03-31 20:52:05 -0300

    Add: Layouts and index page

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- <Navbar /> -->
+        <br/>
+        <h1>MAIN</h1>
+        <div class="mx-4 my-2">
+            <div class="card-body">
+                <NuxtPage />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import Navbar from "@/components/global/NavbarComponent.vue";
+    export default {
+        components: {
+            // Navbar
+        },
+    };
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>SECOND</h2>
+</template>
