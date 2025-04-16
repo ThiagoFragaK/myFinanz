0b3177c0845b0733f1a7c2f507e5564a3480f744
ThiagoFragaK <thiagofraga@proton.me>
2025-04-16 17:23:10 -0300

    Add: Global bootstrap import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,16 @@ export default defineNuxtConfig({
         '~/plugins/sweetalert.client.js',
     ], 
     css: [
-        '~/assets/darkly.css',
+        '~/assets/styles.css',
     ],
     app: {
         head: {
+            link: [
+                {
+                  rel: 'stylesheet',
+                  href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+                }
+            ],
             script: [
                 {
                     src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',
