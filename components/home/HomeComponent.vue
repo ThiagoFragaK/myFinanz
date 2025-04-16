615fd58e4dd3db8e9a3e2471e12699d3255dd1f8
ThiagoFragaK <thiagofraga@proton.me>
2025-04-16 17:49:07 -0300

    Add: navbar component Components

---
+++ components/home/HomeComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-2 mt-5">
+            <div class="row mt-4">
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+            <div class="row mt-4">
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+            <div class="row mt-4">
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+            <div class="row mt-4">
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+            <div class="row mt-4">
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+            <div class="row mt-4">
+                <button type="button" class="btn btn-primary">Primary</button>
+            </div>
+        </div>
+    </div>
+</template>
