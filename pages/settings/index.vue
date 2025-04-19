400aa296ab17007540618ffa546fda154b9835ee
ThiagoFragaK <thiagofraga@proton.me>
2025-04-19 20:47:33 -0300

    Add: Setings page

---
+++ pages/settings/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <SettingsComponent />
+</template>
+
+<script>
+    import SettingsComponent from '@/components/settings/SettingsComponent.vue';
+    export default {
+        components: {
+            SettingsComponent,
+        }
+    }
+</script>
