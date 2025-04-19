d800c0d530e3caceecbc124a1d51a9ab437fbd0f
ThiagoFragaK <thiagofraga@proton.me>
2025-04-19 20:49:28 -0300

    Add: Reports and settings components

---
+++ components/reports/ReportsComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container my-5">
+        <div class="card text-bg-primary shadow rounded-3">
+            <div class="card-header fw-semibold">
+                Reports
+            </div>
+            <div class="card-body">
+                <TabsComponent :tabs="tabs">
+                    <template #current>
+                        <div>Home tab content goes here.</div>
+                    </template>
+                    <template #yearly>
+                        <div>Profile tab content goes here.</div>
+                    </template>
+                    <template #comparaison>
+                        <div>Contact tab content goes here.</div>
+                    </template>
+                </TabsComponent>
+            </div>
+        </div>
+    </div>
+  </template>
+
+<script>
+    import TabsComponent from '@/components/global/TabsComponent.vue';
+    export default {
+    components: {
+        TabsComponent,
+    },
+    data() {
+        return {
+            tabs: [
+                { name: "current", label: "Current month" },
+                { name: "yearly", label: "Yearly overview" },
+                { name: "comparaison", label: "Comparaison" },
+            ],
+        };
+    },
+    };
+</script>
---
+++ components/settings/SettingsComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container my-5">
+        <div class="card text-bg-primary shadow rounded-3">
+            <div class="card-header fw-semibold">
+                Settings
+            </div>
+            <div class="card-body">
+                <TabsComponent :tabs="tabs">
+                    <template #main>
+                        <div>Home tab content goes here.</div>
+                    </template>
+                    <template #cards>
+                        <div>Profile tab content goes here.</div>
+                    </template>
+                    <template #salaries>
+                        <div>Contact tab content goes here.</div>
+                    </template>
+                    <template #payments>
+                        <div>Contact tab content goes here.</div>
+                    </template>
+                </TabsComponent>
+            </div>
+        </div>
+    </div>
+  </template>
+
+<script>
+    import TabsComponent from '@/components/global/TabsComponent.vue';
+    export default {
+    components: {
+        TabsComponent,
+    },
+    data() {
+        return {
+            tabs: [
+                { name: "main", label: "Main settings" },
+                { name: "cards", label: "Cards" },
+                { name: "salaries", label: "Salaries" },
+                { name: "payments", label: "Constant payments" },
+            ],
+        };
+    },
+    };
+</script>
