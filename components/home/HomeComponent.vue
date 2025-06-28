389f07dba31ca31429d50a1c776397b31b942fbe
ThiagoFragaK <thiagofraga@proton.me>
2025-06-28 13:41:42 -0300

    Refactor: Using components for the cards in dashboard

--- components/home/HomeComponent.vue
+++ components/home/HomeComponent.vue
@@ -1,28 +1,13 @@
 <template>
-    <div class="row justify-content-center mt-4">
-        <div class="col">
-            <div class="card border-success bg-transparent mb-3">
-                <div class="card-body text-success">
-                    <h5 class="card-title">Success card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-            </div>
+    <div class="row justify-content-center mt-4 d-flex align-items-stretch">
+        <div class="col d-flex">
+            <BalanceCard class="h-100"/>
         </div>
-        <div class="col">
-            <div class="card text-bg-primary mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Primary card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-            </div>
+        <div class="col d-flex">
+            <ExpensesCard class="h-100"/>
         </div>
-        <div class="col">
-            <div class="card border-success bg-transparent mb-3">
-                <div class="card-body text-success">
-                    <h5 class="card-title">Success card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-            </div>          
+        <div class="col d-flex">
+            <SavingsCard class="h-100"/>
         </div>
     </div>
     <div class="row justify-content-center mt-4">
@@ -36,16 +21,15 @@
 </template>
 
 <script>
-    import IncomeModal from "@/components/home/modals/IncomesModal.vue";
-    import ExpensesModal from "@/components/home/modals/ExpensesModal.vue";
-    import PaymentsModal from "@/components/home/modals/PaymentsModal.vue";
-    import SavingsModal from "@/components/home/modals/SavingsModal.vue";
+    import BalanceCard from "@/components/home/cards/BalanceCard.vue";
+    import ExpensesCard from "@/components/home/cards/ExpensesCard.vue";
+    import SavingsCard from "@/components/home/cards/SavingsCard.vue";
+
     export default {
         components: {
-            IncomeModal,
-            ExpensesModal,
-            PaymentsModal,
-            SavingsModal
+            BalanceCard,
+            ExpensesCard,
+            SavingsCard
         },
         data() {
             return {
