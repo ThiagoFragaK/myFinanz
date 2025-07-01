1ea60d83cdc39c59af89d5363c7bab632b99dc68
ThiagoFragaK <thiagofraga@proton.me>
2025-06-30 21:52:02 -0300

    Add: month date format in Dates Helper

--- helpers/Dates.js
+++ helpers/Dates.js
@@ -3,6 +3,10 @@ import { parseISO, format } from 'date-fns';
 export default {
     getFormatedDate(dateString, pattern = 'dd/MM/yyyy HH:mm') {
         if (!dateString) return '';
+        return format(parseISO(dateString), pattern);    },
+
+    getFormatedMonthDate(dateString, pattern = 'dd/MM') {
+        if (!dateString) return '';
         return format(parseISO(dateString), pattern);
     }
 }
