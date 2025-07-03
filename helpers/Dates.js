d91c7ff8814b549a55fca5bbb66925f61eb3b6e8
ThiagoFragaK <thiagofraga@proton.me>
2025-07-03 19:54:11 -0300

    Refactor: Merge together both dates functions

--- helpers/Dates.js
+++ helpers/Dates.js
@@ -3,10 +3,6 @@ import { parseISO, format } from 'date-fns';
 export default {
     getFormatedDate(dateString, pattern = 'dd/MM/yyyy HH:mm') {
         if (!dateString) return '';
-        return format(parseISO(dateString), pattern);    },
-
-    getFormatedMonthDate(dateString, pattern = 'dd/MM') {
-        if (!dateString) return '';
         return format(parseISO(dateString), pattern);
-    }
+    },
 }
