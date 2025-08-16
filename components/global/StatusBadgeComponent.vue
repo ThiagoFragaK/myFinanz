956969557c4af4c9867e8659bb5cd80b869e9594
ThiagoFragaK <thiagofraga@proton.me>
2025-08-16 09:07:22 -0300

    Refactor: Using success in status component

--- components/global/StatusBadgeComponent.vue
+++ components/global/StatusBadgeComponent.vue
@@ -2,7 +2,7 @@
     <div>
         <span
             v-if="status === 1" 
-            class="badge text-bg-secondary"
+            class="badge text-bg-success"
         >
             <div class="text-light">Active</div>
         </span>
