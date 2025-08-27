fcd2551266a4ad688a91079eeb3cd46099dc6601
ThiagoFragaK <thiagofraga@proton.me>
2025-08-26 22:30:56 -0300

    hotfix: using variable in axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default defineNuxtPlugin((nuxtApp) => {
     const api = axios.create({
-        baseURL: 'process.env.NUXT_PUBLIC_API_BASE_URL',
+        baseURL: process.env.NUXT_PUBLIC_API_BASE_URL,
         headers: {
             'Content-Type': 'application/json',
         }
