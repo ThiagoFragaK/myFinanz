b3a5623145aa54349934945bb01799647a9862fd
ThiagoFragaK <thiagofraga@proton.me>
2025-10-25 10:14:57 -0300

    Refactor: validation rules file

--- rules/validationRules.js
+++ rules/validationRules.js
@@ -2,7 +2,7 @@ import { defineRule } from 'vee-validate'
 
 defineRule('required', value => {
     if (!value || String(value).trim() === '') {
-        return 'Este campo é obrigatório.'
+        return "Field is required."
     }
     return true
 })
@@ -11,34 +11,41 @@ defineRule('email', value => {
     if (!value || /^\S+@\S+\.\S+$/.test(value)) {
         return true
     }
-    return 'Insira um e-mail válido.'
+    return "Email isn't valid."
 })
 
 defineRule('min', (value, [min]) => {
     if (value && value.length < min) {
-        return `Mínimo de ${min} caracteres.`
+        return `Minimum of ${min} characters.`
     }
     return true
 })
 
 defineRule('max', (value, [max]) => {
     if (value && value.length > max) {
-        return `Máximo de ${max} caracteres.`
+        return `Maximum of ${max} characters.`
     }
     return true
 })
 
 defineRule('password', value => {
-    if (!value) return 'Senha obrigatória.'
+    if (!value) return 'Password is required.'
     const strong = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
     return strong.test(value)
         ? true
-        : 'A senha deve ter ao menos 8 caracteres, incluindo maiúsculas, minúsculas e números.'
+        : "Password isn't valid. At least 8 characters, lower and upper case characeters and numbers."
 })
 
 defineRule('confirmed', (value, [target], ctx) => {
     if (value === ctx.form[target]) {
         return true
     }
-    return 'Os campos não coincidem.'
+    return "Fields doesn't match."
 })
+
+export const rules = {
+    name: 'required|min:3',
+    type: 'required',
+    limit: 'required|min:1',
+    turnDay: 'required|min:1|max:31'
+}
